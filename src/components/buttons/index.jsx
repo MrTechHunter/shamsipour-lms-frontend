c48823d52e8b95f343610b557614797423f503e9
mahdi-mohamadi <mahdiprog81@gmail.com>
2023-06-15 00:06:43 +0330

    feature: login page

--- src/components/buttons/index.jsx
+++ src/components/buttons/index.jsx
@@ -2,13 +2,23 @@ import { LoadingButton } from "@mui/lab";
 import { Button } from "@mui/material";
 import React from "react";
 
-const Buttons = ({ variant, label, icon, loading, fullWidth }) => {
+const Buttons = ({
+  variant,
+  label,
+  icon,
+  loading,
+  fullWidth,
+  disabled,
+  type,
+}) => {
   if (variant === "normal") {
     return (
       <LoadingButton
         variant={"contained"}
         loading={loading}
         fullWidth={fullWidth}
+        disabled={disabled}
+        type={type}
       >
         <div className="flex flex-nowrap items-center justify-between gap-1">
           <span>{label}</span>
@@ -18,14 +28,14 @@ const Buttons = ({ variant, label, icon, loading, fullWidth }) => {
     );
   } else if (variant === "outlined") {
     return (
-      <Button variant="outlined">
+      <Button variant="outlined" type={type}>
         <span>{label}</span>
         <i className={icon} />
       </Button>
     );
   } else {
     return (
-      <Button variant="text">
+      <Button variant="text" type={type}>
         <span>{label}</span>
         <i className={icon} />
       </Button>
