d421f0bb6d0d756d0171a128b01067aa5556e0a7
mahdi-mohamadi <mahdiprog81@gmail.com>
2023-06-23 18:34:24 +0330

    add loading

--- src/components/loading/fullScreen.tsx
+++ src/components/loading/fullScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FullScreen = () => {
   return (
     <div className="bg-blue_050 w-full h-screen flex justify-center items-center capitalize text-lg text-black_87">
-      در حال بارگذاری...
+      <img src="/assets/loading.gif" alt="loading" width={150} height={150} />
     </div>
   );
 };
